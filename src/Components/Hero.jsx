e1c3b7fadcbf605aada18d88dba051fad105330f
rakibhossainofficial <rkrrakib20@gmail.com>
2025-04-21 23:59:22 +0600

    Hero fully Complete

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
+import React from "react";
 
 const Hero = () => {
   return (
     <>
-    <div className='py-12'>
-      <img className='w-full mx-auto' src="../public/images/banner.png"/>
-    </div>
-      
-    </>
-  )
-}
+      <div className="py-12 ">
+        <img className="w-1/3 mx-auto" src="../public/images/banner.png" />
 
-export default Hero
+        <div className="text-center space-y-4 ">
+          <h1 className="font-thin text-7xl">Browse, Search, View, Buy</h1>
+          <p className="text-gray-500 ">
+            Best place to browse, search, view details and purchase of top
+            flagship phones of the current time - FlagshipFaceOff
+          </p>
+
+          <form className="flex-col md:flex-row p-3 items-center justify-center my-5">
+            <input
+              className=" w-2/3 p-4 text-[17px] shadow-sm  h-12 rounded-md border-gray-500 focus:outline-none focus:shadow-outline  mr-3 mt-0"
+              type="search"
+              placeholder="Search Phone By Name"
+              required
+            />
+
+            <button className="px-4 text-white shadow-sm cursor-pointer h-12 bg-indigo-600 rounded-lg ">Search</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Hero;
